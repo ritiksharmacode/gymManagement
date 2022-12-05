3a2c475f0e92d1f9f00922f52bcd92afeac7bf1b
ritiksharmacode <116292770+ritiksharmacode@users.noreply.github.com>
2022-12-05 20:04:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const start = async () => {
       console.log("connected successfully");
     });
     app.listen(process.env.PORT_NO, () =>
-      console.log("Server started on port " + process.env.PORT_NO)
+      console.log("Server has started on port " + process.env.PORT_NO)
     );
   } catch (error) {
     console.log(error);
