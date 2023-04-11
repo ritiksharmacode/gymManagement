e2f07958b8f5f371a70b331c48179464eda2745f
ritiksharmacode <rritikssharma1920@gmail.com>
2023-04-11 15:20:54 +0530

    msg

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const start = async () => {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
     db.once('open', function () {
-      console.log('connected successfully..');
+      console.log('connected successfully...');
     });
     app.listen(process.env.PORT_NO, () =>
       console.log('Server has started on port ' + process.env.PORT_NO)
