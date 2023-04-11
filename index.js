af9bb2b5659eb251a573b0838f4f0afa3b2792cc
ritiksharmacode <rritikssharma1920@gmail.com>
2023-04-11 15:20:23 +0530

    init

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-import router from "./route/index.route.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import router from './route/index.route.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", router);
+app.use('/', router);
 
 const start = async () => {
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/gymEDiary", {
+    mongoose.connect('mongodb://127.0.0.1:27017/gymEDiary', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "connection error: "));
-    db.once("open", function () {
-      console.log("connected successfully");
+    db.on('error', console.error.bind(console, 'connection error: '));
+    db.once('open', function () {
+      console.log('connected successfully..');
     });
     app.listen(process.env.PORT_NO, () =>
-      console.log("Server has started on port " + process.env.PORT_NO)
+      console.log('Server has started on port ' + process.env.PORT_NO)
     );
   } catch (error) {
     console.log(error);
